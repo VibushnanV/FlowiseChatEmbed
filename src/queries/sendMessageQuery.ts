042955f1c3818927d0457ba209d6c2357e68a2f8
vibushnan <vibhushnan.v@blp.co.in>
2023-09-30 17:55:16 +0530

    new build

--- src/queries/sendMessageQuery.ts
+++ src/queries/sendMessageQuery.ts
@@ -14,14 +14,14 @@ export type MessageRequest = {
     body?: IncomingInput
 }
 
-export const sendMessageQuery = ({ chatflowid, apiHost = 'http://localhost:3000', body }: MessageRequest) =>
+export const sendMessageQuery = ({ chatflowid, apiHost = 'http://35.238.21.52:3003', body }: MessageRequest) =>
     sendRequest<any>({
         method: 'POST',
         url: `${apiHost}/api/v1/prediction/${chatflowid}`,
         body
     })
 
-export const isStreamAvailableQuery = ({ chatflowid, apiHost = 'http://localhost:3000' }: MessageRequest) =>
+export const isStreamAvailableQuery = ({ chatflowid, apiHost = 'http://35.238.21.52:3003' }: MessageRequest) =>
     sendRequest<any>({
         method: 'GET',
         url: `${apiHost}/api/v1/chatflows-streaming/${chatflowid}`,
